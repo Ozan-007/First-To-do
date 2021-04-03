51870d73cd13f4f2f1826c87ff820dd1ef508c6a
Ozan <ozan28061@gmail.com>
2021-04-03 17:00:31 +0200

    alert added

--- todo.js
+++ todo.js
@@ -2,17 +2,27 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+
+
 addToDoButton.addEventListener('click',function()
 {
-    var paragraph = document.createElement("p")
-    paragraph.classList.add('paragraph-styling')
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click',function(){
-        paragraph.style.textDecoration = 'line-through'
+    // alert(inputField.value == Number)
+    if(inputField.value == false){
+        alert("Invalid value");
+    }
+    
+    else{
+        var paragraph = document.createElement("p")
+        paragraph.classList.add('paragraph')
+        paragraph.innerText = inputField.value;
+        toDoContainer.appendChild(paragraph);
+        inputField.value = "";
+        paragraph.addEventListener('click',function(){
+            paragraph.style.textDecoration = 'line-through'
         })
-    paragraph.addEventListener('dblclick',function(){
-        toDoContainer.removeChild(paragraph);
-            })
+        paragraph.addEventListener('dblclick',function(){
+            toDoContainer.removeChild(paragraph);
+        })
+    }
+  
 })
